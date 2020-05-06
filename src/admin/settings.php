ca6794bc7d7314530af9a9af005dabe11f92bb52
tienminhvydev <tienminhvydev@gmail.com>
2020-05-06 13:40:09 +0700

    Cập nhật lại file setting

--- src/admin/settings.php
+++ src/admin/settings.php
@@ -3,13 +3,3 @@
         die('Direct access is not allowed!');
     }
     // Editing this file is for advanced user only
-    
-$db_server = 'localhost';
-$db_name = 'wdcdoc';
-$db_username = 'admin';
-$db_password = 'admin';
-$db_port = 3306;
-$site_addr = 'http://localhost/wdcDocSoftware';
-$sitename = 'wdcDoc';
-$site_email = 'doc@localhost.com';
-$installed = true;
--- src/admin/settings_example.php
+++ src/admin/settings_example.php
@@ -3,3 +3,12 @@
         die('Direct access is not allowed!');
     }
     // Editing this file is for advanced user only
+    $db_server = '';
+    $db_name = '';
+    $db_username = '';
+    $db_password = '';
+    $db_port = '';
+    $site_addr = '';
+    $sitename = '';
+    $site_email = '';
+    $installed = '';
